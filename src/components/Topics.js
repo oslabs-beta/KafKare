1035c10bc8ab6ac51620446f906bd0c655689579
jtbeger <65937748+jtbeger@users.noreply.github.com>
2020-12-16 14:13:55 -0500

    working on grafana dboards

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Topics() {
   return (
     <div className="topics">
-      <h1>Topics</h1>
+      <iframe
+        className="iframe"
+        src="http://localhost:3000/d-solo/2LyxeP1Mk/total-messages?orgId=1&from=1608068487181&to=1608070287181&panelId=2"
+        width="450"
+        height="75"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
