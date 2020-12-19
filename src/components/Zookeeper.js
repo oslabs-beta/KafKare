0451da559395aaf1bfd74bce0c1d3188189e17fe
Wai Fai Lau <wlau8088@gmail.com>
2020-12-19 16:20:50 -0500

    dashboard update

--- src/components/Zookeeper.js
+++ src/components/Zookeeper.js
@@ -4,7 +4,7 @@ function Zookeeper() {
   return (
     <div className="zookeeper">
       <h1><span>Zookeeper: </span>
-      <span class="red">Online </span></h1>
+      <span class="green">Online </span></h1>
     </div>
   );
 }
