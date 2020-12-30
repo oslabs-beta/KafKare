23ac31c6932bb4ce12a6e6fd7a5da7824fd9f303
jtbeger <65937748+jtbeger@users.noreply.github.com>
2020-12-30 15:37:23 -0500

    added Electron App, updated CSS scheme

--- src/components/VirtualMem.js
+++ src/components/VirtualMem.js
@@ -4,7 +4,12 @@ function VirtualMem() {
   return (
     <div className="virtualmem component">
       <h1>Virtual Memory</h1>
-      <iframe src="http://localhost:3000/d-solo/2LyxeP1Mk/total-messages?orgId=1&panelId=14" width="450" height="200" frameborder="0"></iframe>
+      <iframe
+        src="http://localhost:3000/d-solo/2LyxeP1Mk/total-messages?orgId=1&panelId=14"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
