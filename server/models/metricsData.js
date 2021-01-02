220d481eb41d9bda5e148b9745b10c1ced94436e
Jenniel <jenniel@Jenniels-MacBook-Pro.local>
2021-01-02 09:48:11 -0500

    added postgress to yml

--- server/models/metricsData.js
+++ server/models/metricsData.js
@@ -1,32 +1,3 @@
-// const { Pool, Client} = require('pg');  
-
-
-
-// const PG_URI = 'postgres://zoszwsct:oEVljts38HRlyeNlJQ-u-5o3jdfQsvZN@suleiman.db.elephantsql.com:5432/zoszwsct'; 
-
-// const pool = new Pool({
-//         PG_URI,
-//   });
-
-// pool.query('SELECT NOW()', (err, res) => {
-//       console.log(err, res)
-//       pool.end()
-//   }); 
-  
-// const client = new Client({
-//             PG_URI,
-//   })
-//   client.connect()
-
-//   client.query('SELECT NOW()', (err, res) => {
-//       console.log(err, res)
-//       client.end()
-//   })
-  
-
-
-
-
 const { Pool, Client} = require('pg');  
 const pgUrl =  'postgres://zoszwsct:3VvqfB9_GFyMZULPbXOAW3pjXt8aqVFF@suleiman.db.elephantsql.com:5432/zoszwsct'
 const client = new Client(pgUrl) ; 
@@ -36,10 +7,9 @@ async function connect(client) {
         await client.connect()
         console.log('Client Connected ')
 
-        const {rows} = await client.query('SELECT * FROM topics_data')
+        const {rows} = await client.query('SELECT * FROM Metrics')
         console.table(rows)
         await client.end()
-
     }
     catch(ex){
         console.log('Some error' + ex)
