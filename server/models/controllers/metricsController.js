3a2f0d4c2087196347969ccabd7899fd489ce566
Jenniel <jenniel@Jenniels-MacBook-Pro.local>
2021-01-02 15:18:12 -0500

    continuing to work on the db

--- server/models/controllers/metricsController.js
+++ server/models/controllers/metricsController.js
@@ -1,5 +1,5 @@
 const db = require('../metricsData'); 
-
+//insert users into the users table
 const text =   `INSTERT INTO  users(name, email, topic) VALUES($1, $2, $3)`
 const values = [name, email, topic]; 
 
@@ -10,12 +10,26 @@ try {
     console.log(err.stack)
 }; 
 
+
+//insert data into the dashboard metrics
 const dashBoardMetrics = `INSERT INTO Metrics (Date, cpu, iowait, idle) VALUES($1, $2 ,$3 ,$4 )`
 const dashBoardValues  = ['Date', 'cpu', 'iowait', 'idle'];
 
 try {
     const res = client.query(dashBoardMetrics, dashBoardValues)
     console.log(res.rows)
+} catch (err) {
+    console.log(err.stack)
+}
+
+
+//insert data into the the topics_data table
+const topic = `INSERT INTO topics_data (id, topic, date) VALUES($1, $2, $3)`
+const topicValues = ['id', 'topic', 'date ']; 
+
+try {
+    const res = client.query(topic, topicValues)
+    console.log(res.rows[0])
 } catch (err) {
     console.log(err.stack)
 }
