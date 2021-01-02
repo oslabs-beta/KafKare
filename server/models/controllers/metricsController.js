b35cd63d576b5c762a9fcc6517e2d1e94b88add9
Jenniel <jenniel@Jenniels-MacBook-Pro.local>
2021-01-02 15:35:43 -0500

    db-coninued

--- server/models/controllers/metricsController.js
+++ server/models/controllers/metricsController.js
@@ -1,14 +1,39 @@
 const db = require('../metricsData'); 
 //insert users into the users table
-const text =   `INSTERT INTO  users(name, email, topic) VALUES($1, $2, $3)`
-const values = [name, email, topic]; 
+const users =   `INSTERT INTO  users(name, email, topic) VALUES($1, $2, $3)`
+const usersValues = [name, email, topic]; 
 
 try {
-    const res = await client.query(text, values)
+    const res = await client.query(users, usersValues)
     console.log(res.rows[0])
 } catch (err) {
     console.log(err.stack)
 }; 
+
+const userMetrics = `INSERT INTO usermetrics (userNAme,
+userId, 
+userCPU , 
+userMem , 
+consumerLag,
+messages,
+numOfBrokers, 
+systemHealth ) VALUES (
+    $1, $2 ,$3 ,$4, $5, $6 ,$7 ,$8  
+)`
+const userMetricsValues = ['userId', 
+   ' userCPU' , 
+   ' userMem' , 
+   ' consumerLag',
+    'messages',
+    'numOfBrokers', 
+   ' systemHealth'];
+
+   try {
+       const res = await.client.quey(userMetrics, userMetricsValues)
+       console.log(res.rows[0])
+   } catch(err) {
+       console.log(err)
+   }
 
 
 //insert data into the dashboard metrics
@@ -17,11 +42,10 @@ const dashBoardValues  = ['Date', 'cpu', 'iowait', 'idle'];
 
 try {
     const res = client.query(dashBoardMetrics, dashBoardValues)
-    console.log(res.rows)
+    console.log(res.rows[0])
 } catch (err) {
     console.log(err.stack)
-}
-
+};
 
 //insert data into the the topics_data table
 const topic = `INSERT INTO topics_data (id, topic, date) VALUES($1, $2, $3)`
@@ -32,4 +56,8 @@ try {
     console.log(res.rows[0])
 } catch (err) {
     console.log(err.stack)
-}
+};
+
+
+
+
